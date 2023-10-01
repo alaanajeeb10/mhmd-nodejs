849579544549d374533ce8c28d17ee355e03cc00
alaanajeeb10 <131193760+alaanajeeb10@users.noreply.github.com>
2023-10-01 15:36:43 +0300

    Add files via upload

---
+++ employees node js.sql
@@ -0,0 +1,29 @@
+
+-- מבנה טבלה עבור טבלה `employess`
+--
+CREATE TABLE `employees` (
+  `employee_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `First_name` varchar(250) NOT NULL,
+  `Last_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- הוצאת מידע עבור טבלה `employess`
+--
+
+INSERT INTO `employess` (`Firstname`, `lastname`, `employe id`) VALUES
+('mhmd', 'alaa', 0);
+
+-- --------------------------------------------------------
+
+--
+-- מבנה טבלה עבור טבלה `shift`
+--
+
+CREATE TABLE `shift` (
+  `employeesid` int(11) NOT NULL,
+  `entrance` time NOT NULL,
+  `output` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
